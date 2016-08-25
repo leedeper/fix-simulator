099ac42d13b98e13a0cecfaf0115b4b2c3cc0154
leedeper <lyziuu@gmail.com>
2016-08-25 17:11:51 +0800

    Create project

---
+++ src/main/java/com/leedeper/fixsimultor/ISession.java
@@ -0,0 +1,14 @@
+/*
+ * Created by Leedeper on Aug 21, 2016.
+ * All rights reserved.
+ */
+package com.leedeper.fixsimultor;
+/**
+ * 
+ * @author Leedeper
+ *
+ */
+public interface ISession {
+
+}
+
---
+++ src/main/java/com/leedeper/fixsimultor/ITrigger.java
@@ -0,0 +1,15 @@
+/*
+ * Created by Leedeper on Aug 21, 2016.
+ * All rights reserved.
+ */
+package com.leedeper.fixsimultor;
+/**
+ * 
+ * @author Leedeper
+ *
+ */
+public interface ITrigger {
+	public void whenMsgType(String ...types);
+	
+}
+
